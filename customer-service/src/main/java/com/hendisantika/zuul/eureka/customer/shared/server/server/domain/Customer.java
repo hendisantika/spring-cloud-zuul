7ebb4628093ac87741fce00b2633efec80f25f58
Hendi Santika <hendisantika@gmail.com>
2017-08-31 06:58:21 +0700

    #7 - Added customer service codes

---
+++ customer-service/src/main/java/com/hendisantika/zuul/eureka/customer/shared/server/server/domain/Customer.java
@@ -0,0 +1,51 @@
+package com.hendisantika.zuul.eureka.customer.shared.server.server.domain;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/30/17
+ * Time: 6:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ customer-service/src/main/java/com/hendisantika/zuul/eureka/customer/shared/server/server/rest/CustomerController.java
@@ -0,0 +1,42 @@
+package com.hendisantika.zuul.eureka.customer.shared.server.server.rest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/30/17
+ * Time: 6:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import com.hendisantika.zuul.eureka.customer.shared.server.server.domain.Customer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CustomerController {
+
+    private List<Customer> customers;
+
+    public CustomerController() {
+        customers = new LinkedList<>();
+        customers.add(new Customer(1, "Naruto", "Test"));
+        customers.add(new Customer(2, "Sasuke", "Test2"));
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET, produces = "application/json")     public Customer getCustomer(@PathVariable int id) {
+
+        Optional<Customer> customer = customers.stream().filter(customer1 -> customer1.getId() == id).findFirst();
+
+        return customer.get();
+
+    }
+
+}
